396ca4b08cb6bb36833e6f35b7eacf2d679c3333
Dan Bennett <naebeth@googlemail.com>
2012-10-30 17:08:32 +0100

    very first version of OpenTrack

--- track.php
+++ track.php
@@ -1,13 +1,69 @@
 <?php
 
-include 'lib/opentrack.class.php';
-$tracker = new OpenTrack();
-$tracker->dbConnect("address", "username", "password", "databasename", "tablename");
-if(isset($_GET['test'])){
-	$results = $tracker->track(true);
-	echo "<pre>" . print_r($results, true) . "</pre>";
-} else {
-	$tracker->track();
+/*
+ Return the image by default so that, even if the script fails, the user isn't aware that
+ anything has gone wrong (broken image symbol).
+*/
+header("Content-Type: image/png");
+echo file_get_contents('img.png');
+
+if((isset($_GET['campaign']) && !empty($_GET['campaign'])) && (isset($_GET['email']) && !empty($_GET['email'])) && $_GET['email'] != "$%cust_email%$"){
+	$email = $_GET['email'];
+	$campaign = $_GET['campaign'];
+
+	/*
+	 HostEurope currently doesn't have PHP set to use browscap, but I wrote some code to take
+	 advantage of it in case that fact changes, as browscap is a very powerful detection method
+	*/
+	$browscap = ini_get('browscap');
+	if(!empty($browscap) && !is_null($browscap) && $browscap !== false){
+		$agent = get_browser($_SERVER['HTTP_USER_AGENT'], true);
+		foreach ($agent as $key => $value) {
+			switch ($key) {
+				case 'comment':
+					$client = ($value == "Default Browser") ? NULL : ", '".$value."'";
+					break;
+				case 'platform':
+					$platform = ($value == "unknown") ? NULL : ", '".$value."'";
+					break;
+				case 'platform_description':
+					$platform = ($value == "unknown") ? NULL : ", '".$value."'";
+					break;
+			}
+		}
+	/*
+	 If browscap is not available, use a simple 3rd party (freely distributable under GNU Lesser
+	 General Public License) which uses a series of regex checks on the user agent.
+	 This script only returns either mobile, desktop, tv or tablet, but that's still better than
+	 nothing. It assumes the device is 'mobile' and works up from there.
+	*/
+	} else {
+		if(file_exists('categorizr.php')){
+			@include("categorizr.php");
+			$device = categorizr();
+			$agent = "categorizr";
+			$platform = ", '".$device."'";
+		}
+	}
+
+	/*
+	 Connect to the database (switch the $db line depending on whether you're testing or live) and
+	 dump the data into the database.
+	 If you add/change any browser detection methods set $agent to a unique string for all except
+	 browscap so that it will always be the default.
+	*/
+	$db = mysql_connect("localhost", "dbu1025481", "fusiondb4u");
+	// $db = mysql_connect("localhost", "root", "");
+	mysql_select_db("db1025481-fusion", $db);
+	if(isset($agent)){
+		if($agent == 'categorizr'){
+			$results = mysql_query("INSERT INTO `email_tracking` (`email`, `campaign`, `platform`) VALUES ('".$email."', '".$campaign."'".$platform.");");
+		} else {
+			$results = mysql_query("INSERT INTO `email_tracking` (`email`, `campaign`, `client`, `platform`) VALUES ('".$email."', '".$campaign."'".$client.$platform.");");
+		}
+	} else {
+		$results = mysql_query("INSERT INTO `email_tracking` (`email`, `campaign`) VALUES ('".$email."', '".$campaign."');");
+	}
 }
 
 ?>
