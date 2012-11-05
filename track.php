4b91a4dd42dc67f61a78d4cdb9f53dfea2dca73e
Dan Bennett <naebeth@googlemail.com>
2012-11-05 14:47:47 +0100

    d

--- track.php
+++ track.php
@@ -3,8 +3,7 @@
 $test = (isset($_GET['test'])) ? true : false;
 include 'lib/OpenTrack.php';
 $tracker = new OpenTrack($test);
-$tracker->dbConnect("localhost", "dbu1025481", "fusiondb4u", "db1025481-fusion", "email_tracking");
-// $tracker->dbConnect("address", "username", "password", "databasename", "tablename");
+$tracker->dbConnect("address", "username", "password", "databasename", "tablename");
 $tracker->logsDirOrganize(false);
 $results = $tracker->track();
 if($test){
