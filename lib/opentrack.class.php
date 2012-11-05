e7f5c774580a04dbe29420e56885e9cff42f3d57
Dan Bennett <naebeth@googlemail.com>
2012-11-05 10:28:12 +0100

    added PHPDocumentor comments

--- lib/opentrack.class.php
+++ lib/opentrack.class.php
@@ -1,19 +1,137 @@
 <?php
 
+/**
+* OpenTrack - a simple class to gather information on the people who open your emails
+* 
+* This class is designed to be used in email newsletters or other group emails in order
+* to collect information on the people who open the email. It cannot track how long the
+* email was open for or whether or not the user clicked any links inside the email, but
+* by default it will add a new record to a MySQL database containing the email address,
+* email campaign, date/time and the device used to view the email.
+* 
+* This class requires access to a MySQL database in order to function.
+* Device detection requires one of the following:
+* * browscap.ini (http://de3.php.net/manual/en/function.get-browser.php)
+* * GaretJax's PHPBrowscap class (https://github.com/GaretJax/phpbrowscap)
+* * bjankord's Categorizr class (https://github.com/bjankord/Categorizr)
+* 
+*
+* @package  OpenTrack
+* @version  1.4
+* @author   Dan Bennett <danielj.bennett@yahoo.com>
+* @license  http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+* @link     https://github.com/Ultrabenosaurus/OpenTrack/
+*/
 class OpenTrack{
+    /**
+     * $dirs - An array of directories needed for the class to run
+     *
+     * @var array
+     *
+     * @access private
+     */
 	private $dirs;
+
+    /**
+     * $device - Whether or not to attempt device detection
+     *
+     * @var boolean
+     *
+     * @access private
+     */
 	private $device;
+
+    /**
+     * $db - The MySQL connection resource
+     *
+     * @var resource
+     *
+     * @access private
+     */
 	private $db;
+
+    /**
+     * $db_addr - MySQL URI to connect to
+     *
+     * @var string
+     *
+     * @access private
+     */
 	private $db_addr;
+
+    /**
+     * $db_user - The username with which to connect
+     *
+     * @var string
+     *
+     * @access private
+     */
 	private $db_user;
+
+    /**
+     * $db_pass - The password with which to connect
+     *
+     * @var string
+     *
+     * @access private
+     */
 	private $db_pass;
+
+    /**
+     * $db_name - The database to connect to
+     *
+     * @var string
+     *
+     * @access private
+     */
 	private $db_name;
+
+    /**
+     * $db_tabl - The table in which to store data
+     *
+     * @var string
+     *
+     * @access private
+     */
 	private $db_tabl;
+
+    /**
+     * $db_fiel - Whether new fields should be added to the table or removed from $this->data
+     *
+     * @var boolean
+     *
+     * @access private
+     */
 	private $db_fiel;
-	private $cache_dir;
+
+    /**
+     * $data - An array containing all collected data
+     *
+     * @var array
+     *
+     * @access private
+     */
 	private $data;
+
+    /**
+     * $test - An array containing various debugging information
+     *
+     * @var array
+     *
+     * @access private
+     */
 	private $test;
 
+	/**
+	 * Construction method - sets up the basic variables needed to start tracking
+	 * 
+	 * @param boolean $_device    Whether or not to attempt device detection.
+	 * @param string  $_cache_dir PHPBrowscap's cache directory (will be appended to the root dir).
+	 * @param mixed   $_db_fiel   Whether data targetting non-existing fields should be deleted
+	 *                            or have the fields created.
+	 *
+	 * @access public
+	 */
 	public function __construct($_device = true, $_cache_dir = 'phpbc_cache/', $_db_fiel = true){
 		error_reporting(~E_NOTICE);
 		$this->dirs = array(
@@ -31,27 +149,75 @@ class OpenTrack{
 		$this->test = array();
 	}
 	
+
+	/**
+	 * Creates a MySQL database connection
+	 * 
+	 * If $_db_name and/or $_db_tabl are omitted, you will need to use dbSwitch() before
+	 * you can start tracking emails.
+	 * 
+	 * @param string $_db_addr MySQL URI.
+	 * @param string $_db_user Username to access the database.
+	 * @param string $_db_pass Password to access the database.
+	 * @param mixed  $_db_name Database to access (string or null).
+	 * @param mixed  $_db_tabl Table to use in the database (string or null).
+	 *
+	 * @access public
+	 */
 	public function dbConnect($_db_addr, $_db_user, $_db_pass, $_db_name = null, $_db_tabl = null){
 		$this->db_addr = $_db_addr;
 		$this->db_user = $_db_user;
 		$this->db_pass = $_db_pass;
 		$this->db = mysql_connect($this->db_addr, $this->db_user, $this->db_pass);
 		
-		if(!is_null($_db_name) && !is_null($_db_tabl)){
+		if((!is_null($_db_name) && !empty($_db_name)) && (!is_null($_db_tabl) && !empty($_db_tabl))){
 			$this->dbSwitch($_db_name, $_db_tabl);
 		}
 	}
 	
+
+	/**
+	 * Closes the database connection
+	 * 
+	 * Allow for easily tracking data to multiple tables/databases.
+	 * 
+	 * @access public
+	 */
 	public function dbDisconnect(){
-		
+		if(!is_null($this->db)){
+			$this->db = mysql_close($this->db);
+
+		}
 	}
 	
+
+	/**
+	 * Switch the database/table used for storing data
+	 * 
+	 * Allows for easily tracking data to multiple tables/databases.
+	 * 
+	 * @param mixed $_db_name Database to access (string or null).
+	 * @param mixed $_db_tabl Table to use in the database (string or null).
+	 *
+	 * @access public
+	 */
 	public function dbSwitch($_db_name, $_db_tabl){
 		$this->db_name = $_db_name;
 		$this->db_tabl = $_db_tabl;
 		mysql_select_db($this->db_name, $this->db);
 	}
 	
+
+	/**
+	 * The method to actually collect and store data
+	 * 
+	 * @param boolean $_test Whether or not to enable debugging mode.
+	 *
+	 * @access public
+	 *
+	 * @return mixed $this->test A multi-dimensional associative array of the information collected.
+	 *                           Prevents data being stored in the database.
+	 */
 	public function track($_test = false){
 		$em_camp = explode(',', $this->_getFromQueryString());
 		if($this->device){
@@ -68,20 +234,24 @@ class OpenTrack{
 		}
 	}
 	
+
+	/**
+	 * Extracts the email address and email campaign from the query string.
+	 * 
+	 * @access private
+	 *
+	 * @return string A comma-separated combination of the email address and email campaign.
+	 */
 	private function _getFromQueryString(){
 		return $_GET['email'].",".$_GET['campaign'];
 	}
 	
-	/*
-	 Attempt to use PHP's built-in browscap detection method to get information on the user's device
-	 and client. However, not all hosts support browscap, and those that do may be using an outdated
-	 version, so GaretJax's PHPBrowscap and bjankord's Categorizr classes are supported.
-	 You can force the default browscap to be skipped by including &browscap at the end of the query
-	 string in the image source attribute when using this script.
-	 
-	 PHPBrowscap - https://github.com/GaretJax/phpbrowscap
-	 Categorizr - https://github.com/bjankord/Categorizr
-	*/
+
+	/**
+	 * Attempt to determine the device used to open the email.
+	 * 
+	 * @access private
+	 */
 	private function _getDeviceInfo(){
 		$browscap = isset($_GET['browscap']) ? $_GET['browscap'] : ini_get('browscap');
 		if(!empty($browscap) && !is_null($browscap) && $browscap !== false){
@@ -152,6 +322,17 @@ class OpenTrack{
 		}
 	}
 	
+
+	/**
+	 * Checks the state of the table to be used against the data collected.
+	 * 
+	 * If the table does not exist, it will be created in a state to store the default data collection.
+	 * If data has been collected which targets a non-existing field in the table, it will either be
+	 * removed from $this->data or the class will attempt to add new fields to the table based on the
+	 * values in $this->data.
+	 * 
+	 * @access private
+	 */
 	private function _prepareTable(){
 		$this->_createTable();
 		$field_exists = mysql_query("SHOW COLUMNS FROM `".$this->db_tabl."`;", $this->db);
@@ -170,9 +351,12 @@ class OpenTrack{
 		$this->test['data_final'] = $this->data;
 	}
 	
-	/*
-	 Check if the table exists. If it doesn't, create it with default fields.
-	*/
+
+	/**
+	 * Check for the tables existence, create in a default state if not found.
+	 * 
+	 * @access private
+	 */
 	private function _createTable(){
 		$table_exists = mysql_query("SELECT COUNT(*) FROM `information_schema`.`tables` WHERE `table_schema`='".$this->db_name."' AND `table_name`='".$this->db_tabl."';", $this->db);
 		$table_exists = mysql_fetch_array($table_exists);
@@ -194,10 +378,14 @@ class OpenTrack{
 		}
 	}
 	
-	/*
-	 Loop through all fields in the table, check against keys in $data. If an entry in $data doesn't
-	 have a matching field, attempt to create it based on the type and size of $data's value.
-	*/
+
+	/**
+	 * Attempt to add new fields in the table if necessary, based on the collected data.
+	 * 
+	 * @param array $diff An array of the target fields present in $this->data but not in the table.
+	 *
+	 * @access private
+	 */
 	private function _addFields($diff){
 		if(count($diff) > 0){
 			foreach ($diff as $key => $value){
@@ -223,10 +411,14 @@ class OpenTrack{
 		}
 	}
 	
-	/*
-	 Loop through all fields in the table, check against keys in $data. If an entry in $data doesn't
-	 have a matching field, remove it from the array.
-	*/
+
+	/**
+	 * Remove information from $this->data which targets non-existing fields in the table.
+	 * 
+	 * @param array $diff An array of the target fields present in $this->data but not in the table.
+	 *
+	 * @access private
+	 */
 	private function _removeFields($diff){
 		if(count($diff) > 0){
 			foreach ($diff as $key => $value) {
@@ -235,11 +427,15 @@ class OpenTrack{
 		}
 	}
 	
-	/*
-	 Prepare collected data for inserting into the database. By using an associative array, this
-	 script is easily expandable for any data you want to collect about your email viewers.
-	 Email and Campaign are excluded from $data as they are the main point of this script.
-	*/
+
+	/**
+	 * Prepare the collected data in MySQL INSERT format.
+	 * 
+	 * @param string $email    The email address of the user who opened the email.
+	 * @param string $campaign The campaign to which the opened email belongs.
+	 *
+	 * @access private
+	 */
 	private function _prepareData($email, $campaign){
 		$fields = "(`email`, `campaign`";
 		$values = "('".$email."', '".$campaign."'";
@@ -264,10 +460,27 @@ class OpenTrack{
 		$this->data['values'] = $values;
 	}
 	
+
+	/**
+	 * Insert the collected data into the database.
+	 * 
+	 * @param string $fields The fields into which data is to be inserted.
+	 * @param string $values The data to be inserted.
+	 *
+	 * @access private
+	 */
 	private function _insertData($fields, $values){
 		$response = mysql_query("INSERT INTO `".$this->db_tabl."` ".$this->data['fields']." VALUES ".$this->data['values'].";", $this->db);
 	}
 	
+
+	/**
+	 * Attempt to log errors so that the user never sees a "broken image" icon.
+	 * 
+	 * @param string $info The error to log.
+	 *
+	 * @access private
+	 */
 	private function _log($info){
 		if(!is_dir($this->dirs['logs'])){
 			mkdir($this->dirs['logs'], true);
--- track.php
+++ track.php
@@ -1,8 +1,13 @@
 <?php
 
+// include the class, instantiate a new OpenTrack object
 include 'lib/opentrack.class.php';
 $tracker = new OpenTrack();
+
+// connect to the MySQL URI, access a specific database and table
 $tracker->dbConnect("address", "username", "password", "databasename", "tablename");
+
+// if running as a test, print results otherwise store information in the database
 if(isset($_GET['test'])){
 	$results = $tracker->track(true);
 	echo "<pre>" . print_r($results, true) . "</pre>";
